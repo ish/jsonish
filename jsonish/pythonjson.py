fe088332dccc6ae317fa8836065d071acf62fd5b
Tim Parkin <tim.parkin@jdi-associates.com>
2009-02-24 22:23:47 +0000

    Added Decimal type

--- jsonish/pythonjson.py
+++ jsonish/pythonjson.py
@@ -1,45 +1,55 @@
 import datetime
 from jsonish.json import json
+from decimal import Decimal
 
 
 def date_to_dict(obj):
-    return {"year": obj.year, "month": obj.month, "day": obj.day}
+    return {'year': obj.year, 'month': obj.month, 'day': obj.day}
 
 def date_from_dict(obj):
-    return datetime.date(obj["year"], obj["month"], obj["day"])
+    return datetime.date(obj['year'], obj['month'], obj['day'])
 
 def time_to_dict(obj):
-    return {"hour": obj.hour, "minute": obj.minute, "second": obj.second,
-            "microsecond": obj.microsecond}
+    return {'hour': obj.hour, 'minute': obj.minute, 'second': obj.second,
+            'microsecond': obj.microsecond}
 
 def time_from_dict(obj):
-    return datetime.time(obj["hour"], obj["minute"], obj["second"], obj["microsecond"])
+    return datetime.time(obj['hour'], obj['minute'], obj['second'], obj['microsecond'])
 
 def datetime_to_dict(obj):
-    return {"year": obj.year, "month": obj.month, "day": obj.day, "hour": obj.hour, "minute": obj.minute, "second": obj.second,
-            "microsecond": obj.microsecond}
+    return {'year': obj.year, 'month': obj.month, 'day': obj.day, 'hour': obj.hour, 'minute': obj.minute, 'second': obj.second,
+            'microsecond': obj.microsecond}
 
 def datetime_from_dict(obj):
-    return datetime.datetime(obj["year"], obj["month"], obj["day"], obj["hour"], obj["minute"], obj["second"], obj["microsecond"])
+    return datetime.datetime(obj['year'], obj['month'], obj['day'], obj['hour'], obj['minute'], obj['second'], obj['microsecond'])
+
+def decimal_from_dict(obj):
+    return Decimal(obj['value'])
+
+def decimal_to_dict(obj):
+    return {'value': str(obj)}
 
 
 
-json.register_type(datetime.date, date_to_dict, date_from_dict, "date")
-json.register_type(datetime.time, time_to_dict, time_from_dict, "time")
-json.register_type(datetime.datetime, datetime_to_dict, datetime_from_dict, "datetime")
+json.register_type(datetime.date, date_to_dict, date_from_dict, 'date')
+json.register_type(datetime.time, time_to_dict, time_from_dict, 'time')
+json.register_type(datetime.datetime, datetime_to_dict, datetime_from_dict, 'datetime')
+json.register_type(Decimal, decimal_to_dict, decimal_from_dict, 'decimal')
 dumps = json.dumps
 loads = json.loads
 
 decode_mapping = {
-    "datetime": datetime_from_dict,
-    "date": date_from_dict,
-    "time": time_from_dict,
+    'datetime': datetime_from_dict,
+    'date': date_from_dict,
+    'time': time_from_dict,
+    'decimal': decimal_from_dict,
     }
 
 encode_mapping = {
     datetime.datetime: ('datetime',datetime_to_dict),
     datetime.date: ('date',date_to_dict),
     datetime.time: ('time',time_to_dict),
+    Decimal: ('decimal',decimal_to_dict),
     }
 
 def encode_obj_to_dict(obj, mapping=encode_mapping):
@@ -53,7 +63,7 @@ def encode_obj_to_dict(obj, mapping=encode_mapping):
 def decode_obj_from_dict(d, mapping=decode_mapping):
     if not isinstance(d, dict):
         return d
-    name = d.get("__type__")
+    name = d.get('__type__')
     decode = mapping.get(name, None)
     if decode is None:
         return d
